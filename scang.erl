ac35ffd334347df24b39443676bff3d4b5454eb3
Justin Fay <mail@justinfay.me>
2015-05-22 12:20:28 +0100

    Add `read_from_tokens`; Add `DEBUG` macro; Add stub `atom`

--- scang.erl
+++ scang.erl
@@ -1,9 +1,28 @@
 -module(scang).
--export([tokenize/1]).
+-export([tokenize/1, read_from_tokens/1]).
+
+-ifndef(PRINT).
+-define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p ~p~n", [?MODULE, ?LINE, ??Var, Var])).
+-endif.
 
 tokenize(Chars) ->
+    % Split a string into a list of tokens by.
     string:tokens(
         re:replace(
             re:replace(Chars, "[(]", " ( ", [global, {return, list}]),
             "[)]", " ) ", [global, {return, list}]),
         " ").
+
+atom(Token) ->
+    % TODO
+    Token.
+
+read_from_tokens(Tokens) -> read_from_tokens(Tokens, []).
+read_from_tokens([H|T], Expression) ->
+    case H of
+        ")" -> {Expression, T};
+        "(" -> {Parsed, Tokens} = read_from_tokens(T),
+	    read_from_tokens(Tokens, lists:append(Expression, [Parsed]));
+        Token -> read_from_tokens(T, lists:append(Expression, [atom(Token)]))
+    end;
+read_from_tokens([], [H|_]) -> H.
--- test.erl
+++ test.erl
@@ -3,6 +3,7 @@
 
 test() ->
     test_tokenize(),
+    test_read_from_tokens(),
     tests_passed.
 
 test_tokenize() ->
@@ -10,3 +11,10 @@ test_tokenize() ->
     Program = [
         "(", "begin", "(", "define", "r", "10", ")",
         "(", "*", "pi", "(", "*", "r", "r", ")", ")", ")"].
+
+test_read_from_tokens() ->
+    Tokens = scang:read_from_tokens([
+        "(", "begin", "(", "define", "r", "10", ")",
+        "(", "*", "pi", "(", "*", "r", "r", ")", ")", ")"]),
+    Tokens = ["begin", ["define", "r", "10"], ["*", "pi", ["*", "r", "r"]]].
+
